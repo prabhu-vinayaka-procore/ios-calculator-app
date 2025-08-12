3382ea2f325c03215a63ea798a5b35c4ed6c6f16
vinayaka.prabhu@procore.com <vinayaka.prabhu@procore.com>
2025-08-12 11:44:02 +0530

    Initial Commit

---
+++ my-calculator/my_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_calculatorApp.swift
+//  my-calculator
+//
+//  Created by Vinayaka Prabhu on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct my_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my-calculatorTests/my_calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  my_calculatorTests.swift
+//  my-calculatorTests
+//
+//  Created by Vinayaka Prabhu on 12/08/25.
+//
+
+import Testing
+@testable import my_calculator
+
+struct my_calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
